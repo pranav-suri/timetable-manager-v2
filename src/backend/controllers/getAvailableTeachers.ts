38b8991d44f901b0b248e2cf8ee1ef0ecb2f465d
PranavGitHubAcc <pranavmahajan18@gmail.com>
2024-03-23 17:43:53 +0530

    added subtration logic to getAvailableTeacher

--- src/backend/controllers/getAvailableTeachers.ts
+++ src/backend/controllers/getAvailableTeachers.ts
@@ -1,20 +1,27 @@
+import { Json } from "sequelize/lib/utils";
 import { Teacher } from "../database";
 import getAllTeachers from "./getAllTeachers";
 
-async function getAvailableTeachers
-    (slotId: string | number, subjectId: string|number) {
-        const subjectTeachers = await getAllTeachers(subjectId);
-        const teachers = await Teacher.findAll({
-            include: [
-                {
-                    association: "SlotData",
-                    where: { slotId: slotId },
-                },
-            ],
-        });
+async function getAvailableTeachers(slotId: string | number, subjectId: string | number) {
+    const subjectTeachers = await getAllTeachers(subjectId);
+    console.log(JSON.stringify(subjectTeachers));
 
-    return teachers;
+    const teachers = await Teacher.findAll({
+        include: [
+            {
+                association: "SlotData",
+                where: { slotId: slotId },
+            },
+        ],
+    });
+
+    console.log(JSON.stringify(teachers));
     // add subtraction logic
+    const availableTeachers = subjectTeachers.filter(
+        (subjectTeacher) => !teachers.some((teacher) => teacher.id === subjectTeacher.id)
+    );
+    console.log(JSON.stringify(availableTeachers));
+    return availableTeachers;
 }
 
 export default getAvailableTeachers;
