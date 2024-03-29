3e741faad8f2846ef23e269dfb55cd1f3fde551b
Pranav Suri <pranavsuri4@gmail.com>
2024-03-29 23:24:11 +0530

    Removed classroomTimetable path from available and Restructured on error return in available.ts

--- src/backend/api/routes/available.ts
+++ src/backend/api/routes/available.ts
@@ -5,20 +5,11 @@ import { getAvailableClassrooms, getAvailableTeachers, getTimetable } from "../.
 const app = new Elysia();
 app.use(cors({ methods: ["GET", "POST"] }));
 
-app.get("/classroomTimetable", async (req) => {
-    const { classroomId } = req.query;
-    if (!classroomId) {
-        req.set.status = 400;
-        return "classroomId is required.";
-    }
-    return { Timetable: await getTimetable(classroomId, "classroom") };
-});
-
 app.get("/availableTeachers", async (req) => {
     const { subjectId, slotId } = req.query;
     if (!subjectId || !slotId) {
         req.set.status = 400;
-        return "subjectId and slotId are required.";
+        return { error: { message: "subjectId and slotId are required." } };
     }
     return { teachers: await getAvailableTeachers(slotId, subjectId) };
 });
@@ -27,7 +18,7 @@ app.get("/availableClassrooms", async (req) => {
     const { subjectId, slotId } = req.query;
     if (!subjectId || !slotId) {
         req.set.status = 400;
-        return "subjectId and slotId are required.";
+        return { error: { message: "subjectId and slotId are required." } };
     }
     return { classrooms: await getAvailableClassrooms(slotId, subjectId) };
 });
