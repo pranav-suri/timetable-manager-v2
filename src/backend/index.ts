48bb1bbbe9a078a4ee3869bd7065620086619d2b
Pramit Sharma <pramitsmusic@gmail.com>
2024-03-30 01:12:01 +0530

    Refactor backend index.ts file

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,30 +1,16 @@
 import Elysia from "elysia";
 import cors from "@elysiajs/cors";
-import { AcademicYear, Batch, Department, Division, Subdivision } from "./database";
-import { getTimetable } from "./controllers";
-import timetable from "./api/routes/timetable";
+import { available, getTables, timetable } from "./api/routes";
+import sampleDataUpload from "./api/upload/sampleUpload";
 const app = new Elysia();
 
 app.use(timetable);
+app.use(getTables);
+app.use(available);
 app.use(cors({ methods: ["GET"] }));
 
 app.get("/", () => {
     return { message: `${Date.now()}` };
-});
-
-app.post("/upload/:type", async (ctx) => {
-    const { type } = ctx.params;
-    const body = ctx.body;
-
-    if (!body) {
-        ctx.set.status = 400;
-        return { message: "Invalid body" };
-    }
-
-    if (type === "timetable") {
-        return;
-    }
-    return { message: "Invalid type" };
 });
 
 app.listen(3000);
