1ce3b32b9ec235f21b08d42c1296ccac3aa3ce98
Pranav Suri <pranavsuri4@gmail.com>
2024-04-01 03:54:20 +0530

    Updated validator controller

--- src/backend/controllers/validators.ts
+++ src/backend/controllers/validators.ts
@@ -1,27 +1,72 @@
+import { timetable } from "../api/routes";
 import { Classroom, Slot, SlotDataClasses, SlotDatas, Subdivision, Teacher } from "../database";
 import { Sequelize, Op } from "sequelize";
 
 // TODO: Make return type similar in all functions
 
-async function teacherValidator(slotId: number) {
+interface ValidatorInput {
+    slotId: number;
+    teacherIds?: number | number[];
+    classroomIds?: number | number[];
+    subdivisionIds?: number | number[];
+}
+
+// Checks for teacher collisions in a slot
+async function teacherValidator(ids: ValidatorInput) {
+    const { slotId, teacherIds, classroomIds, subdivisionIds } = ids;
+    const whereTeacherClause = teacherIds ? { id: teacherIds } : {};
+
+    const includeClassroomClause = classroomIds
+        ? {
+              association: "SlotDataClasses",
+              where: { ClassroomId: classroomIds },
+              attributes: [],
+              required: true,
+          }
+        : {};
+    const includeSubdivisionClause = subdivisionIds
+        ? {
+              association: "SlotDataSubdivisions",
+              where: { SubdivisionId: subdivisionIds },
+              attributes: [],
+              required: true,
+          }
+        : {};
+
     const teachers = await Teacher.findAll({
         attributes: [["id", "TeacherId"]],
+        where: { ...whereTeacherClause },
         include: [
             {
                 association: "SlotDatas",
                 where: { SlotId: slotId },
+                required: true,
                 attributes: [["id", "SlotDataId"]],
+                include: [{ ...includeClassroomClause }, { ...includeSubdivisionClause }],
             },
         ],
     });
     // Only contains teachers in more than one slotData of a slot.
     const teacherCollisions = teachers.filter((teacher) => teacher.SlotDatas.length > 1);
-    return { slotId, teacherCollisions };
+    return { teacherCollisions };
 }
 
-async function classroomValidator(slotId: number) {
+// Checks for classroom collisions in a slot
+async function classroomValidator(ids: ValidatorInput) {
+    const { slotId, teacherIds, classroomIds, subdivisionIds } = ids;
+    const whereClassroomClause = classroomIds ? { id: classroomIds } : {};
+    const whereTeacherClause = teacherIds ? { TeacherId: teacherIds } : {};
+    const includeSubdivisionClause = subdivisionIds
+        ? {
+              association: "SlotDataSubdivisions",
+              attributes: [],
+              required: true,
+              where: { SubdivisionId: subdivisionIds },
+          }
+        : {};
     const classrooms = await Classroom.findAll({
         attributes: [["id", "ClassroomId"]],
+        where: { ...whereClassroomClause },
         include: [
             {
                 association: "SlotDataClasses",
@@ -30,8 +75,9 @@ async function classroomValidator(slotId: number) {
                 include: [
                     {
                         association: "SlotData",
-                        where: { SlotId: slotId },
+                        where: { SlotId: slotId, ...whereTeacherClause },
                         attributes: [],
+                        include: [{ ...includeSubdivisionClause }],
                     },
                 ],
             },
@@ -40,44 +86,25 @@ async function classroomValidator(slotId: number) {
     const classroomCollisions = classrooms.filter(
         (classroom) => classroom.SlotDataClasses.length > 1,
     );
-    return { slotId, classroomCollisions };
+    return { classroomCollisions };
 }
 
-async function simpleSubdivisionValidator(slotId: number) {
+// Checks for all subdivision collisions in a slot
+async function subdivisionValidator(ids: ValidatorInput) {
+    const { slotId, teacherIds, classroomIds, subdivisionIds } = ids;
+    const whereSubdivisionClause = subdivisionIds ? { id: subdivisionIds } : {};
+    const whereTeacherClause = teacherIds ? { TeacherId: teacherIds } : {};
+    const includeClassroomClause = classroomIds
+        ? {
+              association: "SlotDataClasses",
+              where: { ClassroomId: classroomIds },
+              attributes: [],
+              required: true,
+          }
+        : {};
     const subdivisions = await Subdivision.findAll({
         attributes: ["id"],
-        include: [
-            {
-                association: "SlotDataSubdivisions",
-                attributes: ["SlotDataId"],
-                required: true,
-                include: [
-                    {
-                        association: "SlotData",
-                        where: { SlotId: slotId },
-                        attributes: [],
-                    },
-                ],
-            },
-        ],
-    });
-    const subdivisionCollisions = subdivisions.filter(
-        (subdivision) => subdivision.SlotDataSubdivisions.length > 1,
-    );
-    const finalSubDivisionCollisions = subdivisionCollisions.map((subdivision) => {
-        const updatedSubdivision = {
-            SubdivisionId: subdivision.id,
-            SlotData: subdivision.SlotDataSubdivisions,
-        };
-        return updatedSubdivision;
-    });
-
-    return { slotId, subdivisionCollisions: finalSubDivisionCollisions };
-}
-
-async function complexSubdivisionValidator(slotId: number) {
-    const subdivisions = await Subdivision.findAll({
-        attributes: ["id"],
+        where: { ...whereSubdivisionClause },
         include: [
             {
                 association: "SlotDataSubdivisions",
@@ -86,7 +113,7 @@ async function complexSubdivisionValidator(slotId: number) {
                 include: [
                     {
                         association: "SlotData",
-                        where: { SlotId: slotId },
+                        where: { SlotId: slotId, ...whereTeacherClause },
                         attributes: ["id"],
                         include: [
                             {
@@ -99,6 +126,9 @@ async function complexSubdivisionValidator(slotId: number) {
                                     },
                                 ],
                             },
+                            {
+                                ...includeClassroomClause,
+                            },
                         ],
                     },
                 ],
@@ -107,12 +137,26 @@ async function complexSubdivisionValidator(slotId: number) {
     });
 
     // Only contains subdivisions in more than one slotData of a slot.
-    const subdivisionCollisions = subdivisions.filter(
+    const possibleSubdivCollisions = subdivisions.filter(
         (subdivision) => subdivision.SlotDataSubdivisions.length > 1,
     );
 
+    // Redoing logic without refactoring
+    const subdivCollisions = possibleSubdivCollisions.filter((subdivision) => {
+        const groupIdsSet = new Set(
+            subdivision.SlotDataSubdivisions.map((slotDataSubdivision) => {
+                return slotDataSubdivision.SlotData.Subject.Group.id;
+            }),
+        );
+        // Collision if groupIds are different.
+        if (groupIdsSet.size > 1) return true;
+
+        // Collision if all groupIds are the same but allowSimultaneous is false.
+        if (!subdivision.SlotDataSubdivisions[0].SlotData.allowSimultaneous) return true;
+    });
+
     // Refactoring the data to make it more readable.
-    const finalSubDivisionCollisions = subdivisionCollisions.map((subdivision) => {
+    const refactoredSubdiv = possibleSubdivCollisions.map((subdivision) => {
         const updatedSubdivision = {
             SubdivisionId: subdivision.id,
             SlotDatas: subdivision.SlotDataSubdivisions.map((slotDataSubdivision) => {
@@ -126,70 +170,50 @@ async function complexSubdivisionValidator(slotId: number) {
         };
         return updatedSubdivision;
     });
-
     // Subjects from different groups cannot be at the same time.
-    const groupSubdivCollision = finalSubDivisionCollisions.filter((subdivision) => {
+    const groupSubdivCollision = refactoredSubdiv.filter((subdivision) => {
         const groupIdsSet = new Set(subdivision.SlotDatas.map((slotData) => slotData.GroupId));
-        // Collisions if groupIds are different.
-        if (groupIdsSet.size > 1) return true;
+        // Collision if groupIds are different.
+        if (groupIdsSet.size) return true;
 
-        // Collisions if all groupIds are the same but allowSimultaneous is false.
+        // Collision if all groupIds are the same but allowSimultaneous is false.
         if (!subdivision.SlotDatas[0].allowSimultaneous) return true;
     });
 
-    return { slotId, subdivisionCollisions: groupSubdivCollision };
+    return { subdivisionCollisions: subdivCollisions };
 }
 
-async function slotValidator(slotId: number) {}
-
-async function slotValidatorSQL(slotId: number) {
-    const teacherCollisions = await SlotDatas.findAll({
-        group: ["TeacherId"],
-        attributes: ["TeacherId", [Sequelize.fn("COUNT", "TeacherId"), "count"]],
-        where: { SlotId: slotId },
-        having: Sequelize.where(Sequelize.fn("COUNT", "TeacherId"), { [Op.gt]: 1 }),
-    });
-
-    const classroomCollisions = await SlotDatas.findAll({
-        include: [{ association: "SlotDataClasses", attributes: ["ClassroomId"] }], // include the classrooms
-        group: ["SlotDataClasses.ClassroomId"],
-        attributes: [[Sequelize.fn("COUNT", "SlotDataClasses.ClassroomId"), "count"]],
-        where: { SlotId: slotId },
-        having: Sequelize.where(Sequelize.fn("COUNT", "SlotDataClasses.ClassroomId"), {
-            [Op.gt]: 1,
-        }),
-    });
-
-    const subdivisionCollisions = await SlotDatas.findAll({
-        include: [{ association: "SlotDataSubdivisions", attributes: ["SubdivisionId"] }],
-        group: ["SlotDataSubdivisions.SubdivisionId"],
-        attributes: [[Sequelize.fn("COUNT", "SlotDataSubdivisions.SubdivisionId"), "count"]],
-        where: { SlotId: slotId },
-        having: Sequelize.where(Sequelize.fn("COUNT", "SlotDataSubdivisions.SubdivisionId"), {
-            [Op.gt]: 1,
-        }),
-    });
-
-    if (
-        teacherCollisions.length > 0 ||
-        classroomCollisions.length > 0 ||
-        subdivisionCollisions.length > 0
-    ) return {
-        slotId,
-        teacherCollisions,
-        classroomCollisions,
-        subdivisionCollisions,
-    };
-    return false;
+async function slotValidator(ids: ValidatorInput) {
+    const { teacherCollisions } = await teacherValidator(ids);
+    const { classroomCollisions } = await classroomValidator(ids);
+    const { subdivisionCollisions } = await subdivisionValidator(ids);
+    return { teacherCollisions, classroomCollisions, subdivisionCollisions };
 }
 
-async function timetableValidator(academicYearId: number) {
-    const slots = await Slot.findAll({ where: { AcademicYearId: academicYearId } });
+async function timetablesValidator(
+    academicYearId: number,
+    ids: {
+        teacherIds?: number | number[];
+        classroomIds?: number | number[];
+        subdivisionIds?: number | number[];
+    },
+) {
+    const { teacherIds, classroomIds, subdivisionIds } = ids;
+    const slots = await Slot.findAll({
+        where: { AcademicYearId: academicYearId },
+    });
     const validator = [];
     for (const slot of slots) {
-        const result = await slotValidatorSQL(slot.id);
-        if (result) validator.push(result);
+        const result = {
+            slotId: slot.id,
+            ...(await slotValidator({ slotId: slot.id, teacherIds, classroomIds, subdivisionIds })),
+        };
+        const hasCollision =
+            result.teacherCollisions.length ||
+            result.classroomCollisions.length ||
+            result.subdivisionCollisions.length;
+
+        if (hasCollision) validator.push(result);
     }
     return validator;
 }
-
