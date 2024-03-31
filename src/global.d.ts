38852e69c4962a9cb5d3088517125772cba8da47
Pramit Sharma <pramitsmusic@gmail.com>
2024-04-01 03:08:12 +0530

    Move global.d.ts and add TimetableStructure interface

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,55 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function createModal(text: string): void;
+
+interface TimetableStructure {
+    Timetable: {
+        Slots: [
+            {
+                id: number;
+                day: number;
+                number: number;
+                AcademicYearId: number;
+                SlotDatas: [
+                    {
+                        id: number;
+                        Teacher: {
+                            id: number;
+                            teacherName: string;
+                            teacherEmail: string;
+                        };
+                        Subject: {
+                            id: number;
+                            isLab: boolean;
+                            subjectName: string;
+                        };
+
+                        SlotDataSubdivisions: [
+                            {
+                                id: number;
+                                Subdivision: {
+                                    id: number;
+                                    subdivisionName: string;
+                                };
+                            },
+                        ];
+                        SlotDataClasses: [
+                            {
+                                id: number;
+                                Classroom: {
+                                    id: number;
+                                    classroomName: string;
+                                    isLab: boolean;
+                                };
+                            },
+                        ];
+                    },
+                ];
+            },
+        ];
+    };
+}
+type Timetable = TimetableStructure["Timetable"];
+type Slots = Timetable["Slots"];
+type SlotDatas = Slots[0]["SlotDatas"];
+type SlotDataClasses = SlotDatas[0]["SlotDataClasses"];
+type SlotDataSubdivisions = SlotDatas[0]["SlotDataSubdivisions"];
