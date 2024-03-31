5819fd3aae7c88cd57ab523aa4b3a1dc6387678c
Pramit Sharma <pramitsmusic@gmail.com>
2024-04-01 03:05:53 +0530

    Add Tailwind CSS and ErrorBoundary component

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,21 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import Upload from "./Components/CSV/Upload.tsx";
+
 
 globalThis.createModal = (test) => {
     alert(test);
 };
 
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component {
+    state: ErrorBoundaryState;
+    declare props: React.PropsWithChildren;
+    constructor(props: React.PropsWithChildren) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    static getDerivedStateFromError(_error: unknown) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: unknown, errorInfo: unknown) {
+        // You can also log the error to an error reporting service
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            // You can render any custom fallback UI
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/upload/:name" element={<Upload />} />
-            </Routes>
-        </Router>
+        <ErrorBoundary>
+            <App />
+        </ErrorBoundary>
     </React.StrictMode>,
 );
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/frontend/**/*.{ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
