7458666de085f6863ac7eb1d4fbc5060b7c8a8c4
Pramit Sharma <pramitsmusic@gmail.com>
2024-04-01 03:06:32 +0530

    Update font imports in App component

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react'; // Import the 'React' module
+import React from "react";
 import "./App.css";
 import Timetable from "./Pages/Timetable";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function App() {
     return <Timetable />;
