4a30f553a886c72284a333d64a4e136ee0a8e616
pranav-suri <pranavsuri4@gmail.com>
2024-04-04 03:14:47 +0530

    Logger :Pramit

--- src/backend/logging/index.ts
+++ src/backend/logging/index.ts
@@ -10,7 +10,7 @@ export enum LogLevel {
  */
 export default class Logger {
     private static _getFilePath = (logLevel: LogLevel) => {
-        const basePath = "/var/log/timetable-manager/";
+        const basePath = "./logs/";
         const date = new Date();
         const year = date.getFullYear();
         const month = date.getMonth();
@@ -19,44 +19,51 @@ export default class Logger {
         return basePath + fileName;
     };
 
+    private static createFileIfNotExists = async (filePath: string) => {
+        const file = Bun.file(filePath);
+        if (!file.exists()) {
+            await Bun.write(filePath, "");
+        }
+    };
+
     /**
-     * Function to log messages to the console and to a log file at `/var/log/timetable-manager/`
+     * Function to log messages to the console and to a log file at `./logs/`
      * @param message - The message to be logged.
      * @param logLevel - The log level of the message.
      * @param logToConsole - Whether to log the message to the console. Default is false.
      */
-    static async log(message: string, logLevel: LogLevel, logToConsole: boolean = false) {
+    static log = async (
+        message: string,
+        logLevel: LogLevel,
+        logToConsole: boolean = false,
+        path: string,
+    ) => {
+        const srcPath = path.split("/").slice(-4, path.length).join("/");
+        const messagePrefix = `[${new Date().toISOString()}] : ${srcPath} => `;
         switch (logLevel) {
             case LogLevel.INFO: {
-                if (Bun.env.NODE_ENV === "production") break;
-                if (logToConsole) console.log(message);
-                const infoFile = Bun.file(Logger._getFilePath(logLevel));
-                const infoFileWriter = infoFile.writer();
-                infoFileWriter.write(message);
+                if (Bun.env.NODE_ENV === "production") return;
+                if (logToConsole) console.log(messagePrefix + message);
                 break;
             }
             case LogLevel.DEBUG: {
-                if (Bun.env.NODE_ENV === "production") break;
-                if (logToConsole) console.debug(message);
-                const debugFile = Bun.file(Logger._getFilePath(logLevel));
-                const debugFileWriter = debugFile.writer();
-                debugFileWriter.write(message);
+                if (Bun.env.NODE_ENV === "production") return;
+                if (logToConsole) console.debug(messagePrefix + message);
                 break;
             }
             case LogLevel.ERROR: {
-                if (logToConsole) console.error(message);
-                const errorFile = Bun.file(Logger._getFilePath(logLevel));
-                const errorFileWriter = errorFile.writer();
-                errorFileWriter.write(message);
+                if (logToConsole) console.error(messagePrefix + message);
                 break;
             }
             case LogLevel.WARN: {
-                if (logToConsole) console.warn(message);
-                const warnFile = Bun.file(Logger._getFilePath(logLevel));
-                const warnFileWriter = warnFile.writer();
-                warnFileWriter.write(message);
+                if (logToConsole) console.warn(messagePrefix + message);
                 break;
             }
         }
-    }
+        await this.createFileIfNotExists(Logger._getFilePath(logLevel));
+        await Bun.write(
+            Logger._getFilePath(logLevel),
+            messagePrefix + message + (await Bun.file(Logger._getFilePath(logLevel)).text()) + "\n",
+        );
+    };
 }
