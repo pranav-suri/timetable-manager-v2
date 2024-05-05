c9427b08653fa77ce148ee611c68e6e517ee688c
Pramit Sharma <pramitsmusic@gmail.com>
2024-05-05 15:49:52 +0530

    Exporting buttons

--- src/frontend/Components/Buttons/index.ts
+++ src/frontend/Components/Buttons/index.ts
@@ -1,3 +1,7 @@
 import AcademicYear from "./AcademicYear";
+import Batch from "./Batch";
+import Department from "./Department";
+import Division from "./Division";
+import View from "./View";
 
-export { AcademicYear as Button };
+export { AcademicYear, Batch, Department, Division, View};
