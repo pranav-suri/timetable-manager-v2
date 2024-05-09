2e6c86a87d7a87e60ac308e4ccb03647a08dee70
MatricalDefunkt <pramitsmusic@gmail.com>
2024-05-10 05:21:09 +0530

    Changed button to iconbutton; disabled when insufficient values are selected

--- src/frontend/Components/Buttons/ToggleAllData.tsx
+++ src/frontend/Components/Buttons/ToggleAllData.tsx
@@ -1,20 +1,49 @@
-import React, { SyntheticEvent, useContext } from "react";
-import Switch from "@mui/material/Switch";
-import { FormControlLabel } from "@mui/material";
+import React, { useContext, useEffect, useRef } from "react";
 import { ViewAllDataContext } from "../../context/ViewAllDataContext";
+import { CloseFullscreen, OpenInFull } from "@mui/icons-material";
+import { IconButton, Tooltip } from "@mui/material";
+import { SelectedValuesContext } from "../../context/SelectedValuesContext";
+import { TimetableType } from "../../../utils/types";
 
 export default function ToggleAllData() {
-    const { setViewAllData } = useContext(ViewAllDataContext);
-    const handleChange = (event: SyntheticEvent<Element, Event>, checked: boolean) => {
-        setViewAllData(checked);
+    const { viewAllData, setViewAllData } = useContext(ViewAllDataContext);
+    const { selectedValues } = useContext(SelectedValuesContext);
+    const isDisabled = useRef(true);
+
+    const handleChange = () => {
+        setViewAllData(!viewAllData);
     };
 
+    useEffect(() => {
+        if (selectedValues.timetableType.value === TimetableType.CLASSROOM) {
+            if (!selectedValues.academicYear.selected || !selectedValues.classroom.selected) {
+                isDisabled.current = true;
+            } else isDisabled.current = false;
+        } else if (selectedValues.timetableType.value === TimetableType.DIVISION) {
+            if (
+                !selectedValues.academicYear.selected ||
+                !selectedValues.batch.selected ||
+                !selectedValues.department.selected ||
+                !selectedValues.division.selected
+            ) {
+                isDisabled.current = true;
+            } else {
+                isDisabled.current = false;
+            }
+        } else if (selectedValues.timetableType.value === TimetableType.TEACHER) {
+            if (!selectedValues.academicYear.selected || !selectedValues.teacher.selected) {
+                isDisabled.current = true;
+            } else {
+                isDisabled.current = false;
+            }
+        }
+    }, [selectedValues]);
+
     return (
-        <FormControlLabel
-            onChange={handleChange}
-            control={<Switch />}
-            label="View all data"
-            labelPlacement="bottom"
-        />
+        <Tooltip title="Show All Data">
+            <IconButton onClick={handleChange} disabled={isDisabled.current}>
+                {viewAllData ? <CloseFullscreen /> : <OpenInFull />}
+            </IconButton>
+        </Tooltip>
     );
 }
