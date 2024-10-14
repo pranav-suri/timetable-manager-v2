c1a61f61fb2cbf199fe29f858ac01e73455e3697
pranav-suri <pranavsuri4@gmail.com>
2024-10-14 22:01:16 +0530

    reset branch

--- src/frontend/Components/CSV/index.ts
+++
@@ -1,5 +0,0 @@
-import BatchAndSubdivisionUpload from "./BatchAndSubdivisionUpload";
-import ClassroomUpload from "./ClassroomUpload";
-import SubjectAndTeacherUpload from "./SubjectAndTeacherUpload";
-
-export { BatchAndSubdivisionUpload, ClassroomUpload, SubjectAndTeacherUpload };
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-console.error("Please start the program using the given scripts in package.json");
-process.exit(1);
